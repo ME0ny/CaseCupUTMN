0b6fd07c42659a88732bd613876678050c204650
ME0ny <ipatov.987654321@gmail.com>
2020-04-05 15:02:43 +0300

    django

--- check/views.py
+++ check/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth import login, authenticate, logout
+from django.shortcuts import render, redirect
+from django.template import RequestContext
+from django.http import JsonResponse
 # Create your views here.
+
+def main(request):
+    return render(request, 'site/index.html')
